dcd52fdc823085bf77ab85578aad81bcc17b5297
manningbaxter-lab <manningbaxter@gmail.com>
2026-03-27 21:03:02 +1300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MonkeyBrainTrainerUltimate from "./App.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MonkeyBrainTrainerUltimate />
+    <App />
   </React.StrictMode>
 );
