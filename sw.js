6e081a0c3178d367e8d265cc3234370844c02188
manningbaxter-lab <manningbaxter@gmail.com>
2026-03-27 21:34:48 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-self.addEventListener("fetch", () => {});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.open("mbt").then(cache =>
+      cache.match(event.request).then(
+        res =>
+          res ||
+          fetch(event.request).then(net => {
+            cache.put(event.request, net.clone());
+            return net;
+          })
+      )
+    )
+  );
+});
