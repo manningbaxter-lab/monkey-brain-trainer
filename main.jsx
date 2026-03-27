c5486ddd4f78146b2b6c95b31a1a8135c2861e38
manningbaxter-lab <manningbaxter@gmail.com>
2026-03-27 21:22:08 +1300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js");
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-``
